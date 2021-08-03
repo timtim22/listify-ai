44f918a35fbeb2ab904a59be2660f7b378f3027b
lukew244 <luke.webster86@gmail.com>
2021-08-03 18:09:58 +0100

    updated prompt

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -3,7 +3,7 @@ class Prompt
   def self.correct_grammar(text)
     OpenStruct.new(
       title: "grammar",
-      body: "Write the following text in standard British English based on the following:\n#{text}",
+      body: "Convert the following text to standard British English:\n#{text}",
       stop: "\\n",
       max_tokens: 100,
       temperature: 0.0,

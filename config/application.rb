a683158bc9741dc0abfb26f0c00219eae705fe29
lukew244 <luke.webster86@gmail.com>
2021-08-23 17:13:57 +0100

    add deploy host

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,6 @@ module VoiceChromeBackend
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.hosts << "venturerock-test-459.loca.lt"
+    config.hosts << Rails.application.credentials.deploy_url
   end
 end

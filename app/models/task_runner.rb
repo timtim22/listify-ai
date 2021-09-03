5c5f846a6530adcbd198d50d440a4a53951a1c54
lukew244 <luke.webster86@gmail.com>
2021-09-03 10:23:33 +0100

    Update task runner for new structure

--- app/models/task_runner.rb
+++ app/models/task_runner.rb
@@ -1,24 +1,41 @@
 class TaskRunner
 
-  def self.run_for!(input_object)
+  def self.run_for!(input_object, user)
+    prompt_set = prompt_set_for(input_object.request_type)
+    task_run = create_task_run(user, prompt_set, input_object)
 
-    prompt = Prompt.for(input_object.task_type)
-    prompt_with_query = generate_prompt_with_query(prompt, input_object.input_text)
-    response = GptClient.new.execute_request(prompt_with_query)
-
-    task_run = TaskRun.create!(
-      task_type: task_type,
-      prompt: prompt,
-      input_object: input_object,
-      error: response[:error],
-      user: input_object.user
-    )
-    if task_run.error.nil?
-      task_run.create_results!(response)
+    prompt_set.prompts.map do |prompt|
+      prompt_for_client = prompt_object_from(prompt, input_object)
+      response = GptClient.new.execute_request(prompt_for_client)
+      #response = { success: true, result_text: 'successful response' }
+      create_task_result(task_run, response, prompt)
     end
     task_run
-
   end
+
+  def self.prompt_set_for(request_type)
+    prompt_set = PromptSet.for(request_type)
+    raise "No prompts for #{request_type}" if prompt_set.nil?
+    prompt_set
+  end
+
+  def self.create_task_run(user, prompt_set, input_object)
+    TaskRun.create!(
+      user: user,
+      prompt_set: prompt_set,
+      input_object: input_object,
+    )
+  end
+
+  def self.create_task_result(task_run, response, prompt)
+    task_run.task_results.create!(
+      prompt: prompt,
+      success: response[:success],
+      result_text: response[:result_text],
+      error: response[:error]
+    )
+  end
+
 
   def self.run_for_legacy!(typed_text, recording, task_type)
     input_text = typed_text || TextFromSpeech.new.from(recording)
@@ -32,7 +49,8 @@ class TaskRunner
       response = { result_text: input_text, success: true }
     else
       prompt_with_query = generate_prompt_with_query(prompt, input_text)
-      response = GptClient.new.execute_request(prompt_with_query)
+      #response = GptClient.new.execute_request(prompt_with_query)
+      response = { success: true, result_text: 'successful response' }
     end
 
     task_run = LegacyTaskRun.create!(
@@ -49,18 +67,10 @@ class TaskRunner
 
   private
 
-  def self.generate_prompt_with_query(prompt, input_text)
-    if prompt.nil?
-      raise "Unknown task type!"
-    elsif prompt.gpt_model_id.present?
-      prompt_with_query = prompt.to_object_with_model(input_text)
-      log(prompt_with_query)
-      prompt_with_query
-    else
-      prompt_with_query = prompt.to_object_with_text(input_text)
-      log(prompt_with_query)
-      prompt_with_query
-    end
+  def self.prompt_object_from(prompt, input_object)
+    prompt_object = prompt.generate_with(input_object)
+    log(prompt_object)
+    prompt_object
   end
 
   def self.input_too_short?(input_text)

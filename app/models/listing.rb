f9842700f094b6c69c912a07f9041a36a33a8831
lukew244 <luke.webster86@gmail.com>
2021-09-07 19:38:35 +0100

    Model validation on input length

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,5 @@ class Listing < ApplicationRecord
   has_many :task_runs, as: :input_object, dependent: :destroy
 
   validates :request_type, :input_text, presence: true
+  validates :input_text, length: { minimum: 10, maximum: 400 }
 end

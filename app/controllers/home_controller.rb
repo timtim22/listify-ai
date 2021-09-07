533b79426af35eb4cee914c40faa57cbc6d942d7
lukew244 <luke.webster86@gmail.com>
2021-09-07 19:53:37 +0100

    Temporary landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,5 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if !current_user.admin?
-      redirect_to new_listing_path
-    end
-  end
-
-  def listings
   end
 end

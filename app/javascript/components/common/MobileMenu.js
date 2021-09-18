afb269b1267275b417d2aaca08783db2b6cd9d3b
lukew244 <luke.webster86@gmail.com>
2021-09-18 20:16:59 +0100

    Mobile view fixes

--- app/javascript/components/common/MobileMenu.js
+++ app/javascript/components/common/MobileMenu.js
@@ -1,15 +1,44 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const MobileMenu = () => {
+const MobileMenu = ({ admin }) => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const adminLink = () => {
+    if (admin) {
+      return (
+        <a href="/admin/index"
+          className="block py-2 px-4 text-sm text-gray-700"
+          role="menuitem"
+          tabIndex="-1"
+          id="dd-sign-out-link">
+          Admin
+        </a>
+      )
+    }
+  }
 
   const menuDropdown = () => {
     if (menuOpen) {
       return (
         <div className="absolute right-0 py-1 mt-4 w-48 bg-white rounded-md ring-1 ring-black ring-opacity-5 shadow-lg origin-top-right focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-          <a href="/users/edit" className="block py-2 px-4 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="dd-account-link">Account</a>
-          <a href="/users/sign_out" className="block py-2 px-4 text-sm text-gray-700" data-method="delete" role="menuitem" tabIndex="-1" id="dd-sign-out-link">Sign out</a>
+          <a
+            href="/users/edit"
+            className="block py-2 px-4 text-sm text-gray-700"
+            role="menuitem"
+            tabIndex="-1"
+            id="dd-account-link">
+            Account
+          </a>
+          {adminLink()}
+          <a href="/users/sign_out"
+            className="block py-2 px-4 text-sm text-gray-700"
+            data-method="delete"
+            role="menuitem"
+            tabIndex="-1"
+            id="dd-sign-out-link">
+            Sign out
+          </a>
         </div>
       )
     }

34754436b309734eae9923d12f3672ed0668b829
lukew244 <luke.webster86@gmail.com>
2021-09-21 11:31:14 +0100

    Purge tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+   './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.jsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {

2d0f2194722b76d6967ce08b673b1f7a39b6e676
lukew244 <luke.webster86@gmail.com>
2021-10-11 12:31:40 +0100

    Show in results page if user copied

--- app/helpers/task_runs_helper.rb
+++ app/helpers/task_runs_helper.rb
@@ -2,7 +2,12 @@ module TaskRunsHelper
   def format_results(results)
     results.map do |result|
       if result.safe?
-        simple_format(result.result_text)
+        result_text = simple_format(result.result_text)
+        if result.user_copied?
+        "<div class='text-green-600'>USER COPIED: #{result_text}</div>"
+        else
+          result_text
+        end
       else
         filtered_warning ="FILTERED (#{fail_reason_text(result)}):"
         "<p class='text-red-700'>#{filtered_warning} #{result.result_text}</p>"

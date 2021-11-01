a5dcb3df94e904bb9ea40d31d46bbcdd6a069dab
lukew244 <luke.webster86@gmail.com>
2021-11-01 11:24:31 +0000

    Reorganise listing form hierarchy

--- app/javascript/components/listings/New.js
+++ app/javascript/components/listings/New.js
@@ -1,54 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Form from './Form';
-import Results from '../inputs/Results';
-import ResultsPoll from '../inputs/ResultsPoll';
+import ListingFormContainer from './ListingFormContainer';
+import AreaForm from './AreaForm';
 
 const New = ({ showExample, initialRunsRemaining }) => {
-  const [loading, setLoading] = useState(false);
-  const [results, setResults] = useState([]);
-  const [taskRun, setTaskRun] = useState(null);
-  const [runsRemaining, setRunsRemaining] = useState(initialRunsRemaining);
+ const [runsRemaining, setRunsRemaining] = useState(initialRunsRemaining);
+ const [formType, setFormType] = useState('listing_description');
 
-  useEffect(() => {
-    if (results.length > 0) {
-      window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'});
-    }
-  }, [results])
-
-  const handleNewResults = (newResults) => {
-    const newList = taskRun.is_rerun ? [...results, ...newResults] : newResults;
-    setResults(newList);
-    setLoading(false);
+  const header = () => {
+    return (
+      <div className="flex flex-col items-center w-full">
+        <div className="p-4 w-full font-semibold tracking-wide text-gray-800 bg-purple-100">
+          <p>
+            Thanks for joining our private beta! We're still building our product, and making lots of improvements.
+            Please do give us feedback, it really helps!
+          </p>
+        </div>
+        <h1 className="my-8 text-xl font-medium tracking-wider text-gray-700">Listings Generator</h1>
+        <p className="text-sm">I want to generate a...</p>
+        <div className="flex flex-col items-center py-2 md:flex-row md:justify-center md:items-start md:py-8">
+          {pillButton("Description", "listing_description")}
+          {pillButton("Title", "listing_title")}
+          {pillButton("Area description", "neighbourhood")}
+        </div>
+     </div>
+    )
   }
 
-  const handleTaskRun = (response) => {
-    setRunsRemaining(response.data.runs_remaining);
-    setTaskRun(response.data.task_run);
+  const pillButton = (title, value) => {
+    const selected = formType === value;
+    return (
+      <div
+        className={`mt-6 md:mt-0 ${selected ? 'pill-button-selected' : 'pill-button'}`}
+        onClick={() => { setFormType(value) }}>
+        {title}
+      </div>
+    )
+  }
+
+  const displayForm = () => {
+    if (formType === 'neighbourhood') {
+      return <AreaForm />;
+    } else {
+      return (
+        <ListingFormContainer
+          runsRemaining={runsRemaining}
+          setRunsRemaining={setRunsRemaining}
+          formType={formType}
+          showExample={showExample}
+        />
+      )
+    }
   }
 
   return (
-    <>
-      <Form
-        loading={loading}
-        setLoading={(state) => setLoading(state)}
-        runsRemaining={runsRemaining}
-        onResult={handleTaskRun}
-        showExample={showExample}
-      />
-      <ResultsPoll
-        taskRun={taskRun}
-        onResult={handleNewResults}
-      />
-      <Results
-        loading={loading}
-        setLoading={(state) => setLoading(state)}
-        runsRemaining={runsRemaining}
-        results={results}
-        taskRun={taskRun}
-        onRerun={handleTaskRun}
-      />
-    </>
+    <div className="w-full h-full">
+      {header()}
+      {displayForm()}
+    </div>
   )
 }
 

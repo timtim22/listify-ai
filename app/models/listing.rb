34c02f9cc009e4d79da5291cfb39af46f39f1ed7
lukew244 <luke.webster86@gmail.com>
2021-11-17 14:38:05 +0000

    Quick implementation to display translations in results

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,12 @@ class Listing < ApplicationRecord
 
   validates :request_type, :input_text, presence: true
   validates :input_text, length: { minimum: 10, maximum: 300 }
+
+  def displayable_input_text
+    if input_language != "EN"
+      "Original language: #{input_language}\nOriginal_text:\n #{untranslated_input_text}\n\nTranslated:\n#{input_text}"
+    else
+      input_text
+    end
+  end
 end

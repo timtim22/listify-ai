2656c0352b9edca4c5e3d3c22826bef5517e27e5
lukew244 <luke.webster86@gmail.com>
2021-12-08 23:34:20 +0000

    Simple handling of error in step one request

--- app/workers/gpt_two_step_result_worker.rb
+++ app/workers/gpt_two_step_result_worker.rb
@@ -6,9 +6,20 @@ class GptTwoStepResultWorker
     first_result = GptResult.new.for(task_run_id, prompt_id)
     prompt_set_2 = PromptSet.find(prompt_set_2_id)
     task_run_2 = TaskRun.find(task_run_2_id)
-    task_run_2.input_object.update(input_text: first_result.result_text) # if no error
-    prompt_set_2.prompts.map do |prompt|
-      GptResultWorker.perform_async(task_run_2_id, prompt.id)
+    if first_result.success
+      task_run_2.input_object.update(input_text: first_result.result_text) # if no error
+      prompt_set_2.prompts.map do |prompt|
+        GptResultWorker.perform_async(task_run_2_id, prompt.id)
+      end
+    else
+      prompt_set_2.prompts.map do |prompt|
+        task_run_2.task_results.create!(
+          success: false,
+          prompt: prompt,
+          result_text: "An error occurred processing this request. We're sorry about that. Please try again shortly.",
+          error: "An error occurred in a previous step of this request.".to_json
+        )
+      end
     end
   end
 end

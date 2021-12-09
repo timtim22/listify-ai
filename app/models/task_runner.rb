485b915163189682ca20d8c20d140c088bad5785
lukew244 <luke.webster86@gmail.com>
2021-12-09 16:58:47 +0000

    Remove deprecated code

--- app/models/task_runner.rb
+++ app/models/task_runner.rb
@@ -5,19 +5,12 @@ class TaskRunner
     task_run   = create_task_run(user, prompt_set, input_object, output_language)
 
     generate_gpt_results(task_run, prompt_set)
-    #generate_text_results(task_run, input_object)
     task_run
   end
 
   def generate_gpt_results(task_run, prompt_set)
     prompt_set.prompts.map do |prompt|
       GptResultWorker.perform_async(task_run.id, prompt.id)
-    end
-  end
-
-  def generate_text_results(task_run, input_object)
-    if input_object.class.to_s == "AreaDescription"
-      AreaDescription::Generator.run!(task_run, input_object)
     end
   end
 

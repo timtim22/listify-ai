af86612a3a070d934e5cdbf96cacd5725d048472
lukew244 <luke.webster86@gmail.com>
2022-01-05 20:08:35 +0000

    Add notice component with headless ui

--- app/javascript/components/listings/FormHeader.js
+++ app/javascript/components/listings/FormHeader.js
@@ -25,13 +25,13 @@ const FormHeader = ({ formType, setFormType }) => {
                 <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
               </svg>
             </div>
-          <div>
-            <p className="font-bold">Thanks for joining our private beta! </p>
-            <p className="text-sm">
-              We're still making lots of improvements. Please give us feedback, it really helps!
-            </p>
+            <div>
+              <p className="font-bold">Thanks for joining our private beta! </p>
+              <p className="text-sm">
+                We're still making lots of improvements. Please give us feedback, it really helps!
+              </p>
+            </div>
           </div>
-        </div>
         </div>
       </div>
       <div className="flex flex-col items-center py-2 md:flex-row md:justify-center md:items-start md:py-2">

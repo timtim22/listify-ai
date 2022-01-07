e7580f44a331b456671ccd66166c0a8468106d0e
lukew244 <luke.webster86@gmail.com>
2022-01-07 12:38:34 +0000

    Add firefox css navbar exception (blur not supported)

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,21 +32,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
-    // TODO: make this work to render header bar on ff properly
-    // plugin(function ({ addVariant, e, postcss }) {
-  	//   addVariant('firefox', ({ container, separator }) => {
-    // 		const isFirefoxRule = postcss.atRule({
-    // 		  name: '-moz-document',
-    // 		  params: 'url-prefix()',
-    // 		});
-    // 		isFirefoxRule.append(container.nodes);
-    // 		container.append(isFirefoxRule);
-    // 		isFirefoxRule.walkRules((rule) => {
-    // 		  rule.selector = `.${e(
-    // 			`firefox${separator}${rule.selector.slice(1)}`
-    // 		  )}`;
-    // 		});
-  	//   });
-  	// }),
   ]
 }

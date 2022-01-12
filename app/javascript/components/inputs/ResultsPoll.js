cc9f994d3773a385a05523a7bd9ef4886ce3144d
lukew244 <luke.webster86@gmail.com>
2022-01-12 10:19:21 +0000

    Show partial results if user has waited 25 seconds

--- app/javascript/components/inputs/ResultsPoll.js
+++ app/javascript/components/inputs/ResultsPoll.js
@@ -3,10 +3,14 @@ import PropTypes from 'prop-types';
 import { getRequest } from '../../helpers/requests';
 import IntervalTimer from '../common/IntervalTimer';
 
+const pollInterval = 3;
+
 const ResultsPoll = ({ taskRun, onResult }) => {
   const [polling, setPolling] = useState(false);
   const [results, setResults] = useState([]);
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
 
+  // start polling
   useEffect(() => {
     if (taskRun && !polling) {
       setResults([]);
@@ -14,14 +18,28 @@ const ResultsPoll = ({ taskRun, onResult }) => {
     }
   }, [taskRun]);
 
+  // stop, all results received
   useEffect(() => {
     if (polling && taskRun && results.length >= taskRun.expected_results) {
-      setPolling(false);
-      onResult(results);
+      endPolling();
     }
   }, [results]);
 
+  // stop, some results received and user has waited 25 seconds
+  useEffect(() => {
+    if (polling && taskRun && results.length >= 1 && secondsElapsed > 25) {
+      endPolling();
+    }
+  }, [secondsElapsed]);
+
+  const endPolling = () => {
+    setPolling(false);
+    setSecondsElapsed(0);
+    onResult(results);
+  }
+
   const fetchResults = () => {
+    setSecondsElapsed(secondsElapsed + pollInterval);
     if (taskRun) {
       getRequest(
         `/task_runs/${taskRun.id}/task_results.json`,
@@ -34,7 +52,7 @@ const ResultsPoll = ({ taskRun, onResult }) => {
   if (polling) {
     return (
       <IntervalTimer
-        triggerSeconds={3}
+        triggerSeconds={pollInterval}
         callback={fetchResults}
       />
     )

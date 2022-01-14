c1efd9eb3e9995e603f06a0fecace91e476bcafd
lukew244 <luke.webster86@gmail.com>
2022-01-14 10:58:20 +0000

    Style and refactor switching subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -13,14 +13,14 @@ class SubscriptionsController < ApplicationController
   def create
     current_user.update_card(params[:payment_method_id]) if params[:payment_method_id]
     current_user.subscribe(@plan.stripe_id)
-    redirect_to root_path, notice: "Thanks for subscribing"
+    redirect_to root_path, notice: "Thanks for subscribing!"
   rescue PaymentIncomplete => e
     redirect_to payment_path(e.payment_intent.id)
   end
 
   def edit
     @subscription = current_user.subscription
-    @plans = Plan.all
+    @plans = Plan.where(interval: 'month')
   end
 
   def update

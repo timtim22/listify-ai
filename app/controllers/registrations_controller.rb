3a055a92b3ed3fa5612c1145b1e0e1b6db350317
lukew244 <luke.webster86@gmail.com>
2022-01-19 21:47:40 +0000

    Add recaptcha to signup form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
-    super
-    UserMailer.welcome(resource).deliver_now unless resource.invalid?
+    if passes_recaptcha?
+      super
+      UserMailer.welcome(resource).deliver_now unless resource.invalid?
+    else
+      redirect_to new_user_registration_path, alert: "Failed recaptcha - if this persists please contact hello@listify.ai for support"
+    end
+  end
+
+  private
+  def passes_recaptcha?
+    verified = RecaptchaClient.new.verify_response(params['g-recaptcha-response'])
+    verified["success"]
   end
 end

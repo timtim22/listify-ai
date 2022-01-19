fb7e13544e707da2593a5dfb65257ee8a7a47aa5
lukew244 <luke.webster86@gmail.com>
2022-01-19 10:21:03 +0000

    Trim logo image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

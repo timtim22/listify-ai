d7e290071be355841b2228e2f7659c1e70022874
lukew244 <luke.webster86@gmail.com>
2022-01-20 10:59:31 +0000

    Show more accurate statistics (wip)

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -2,6 +2,16 @@ class Admin::StatisticsController < ApplicationController
   before_action :authenticate_admin
 
   def index
-    @users = User.all.includes(:task_runs)
+    users = User.all
+    @users_with_stats = users.map do |user|
+      stats = SpinCounter.new(user).spin_stats
+      OpenStruct.new(
+        id: user.id,
+        email: user.email,
+        created_at: user.created_at,
+        spins_this_month: stats.spins,
+        monthly_spin_quota: stats.quota
+      )
+    end
   end
 end

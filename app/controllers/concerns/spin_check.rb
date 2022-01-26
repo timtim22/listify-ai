016b8a1f588d9fcdcab869ada5db224a5f6a980e
Luke Webster <luke.webster86@gmail.com>
2022-01-26 10:32:18 +0000

    Remove extra space

--- app/controllers/concerns/spin_check.rb
+++ app/controllers/concerns/spin_check.rb
@@ -4,7 +4,7 @@ module SpinCheck
   def self.runs_remaining(user)
     runs_remaining = user.runs_remaining_today
     raise Errors::UserAccountLocked if user.account_locked?
-    raise Errors::NoSpinsRemaining if runs_remaining  <= 0
+    raise Errors::NoSpinsRemaining if runs_remaining <= 0
     runs_remaining
   end
 end

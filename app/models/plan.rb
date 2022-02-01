0c02c94bfbf2bd0c67f08007ac2086e0d6ce1ba8
Luke Webster <luke.webster86@gmail.com>
2022-02-01 22:13:28 +0000

    Remove minotaur from nav

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -8,6 +8,7 @@ class Plan < ApplicationRecord
   def monthly_spin_cap
     case name.downcase
     when "starter" then 30
+    when "starter_dep" then 30
     when "standard" then 250
     when "enterprise" then 1000
     else 0

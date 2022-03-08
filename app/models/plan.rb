f55b6d34a43ae3501dc439c9a779bd9d06336a8d
Luke Webster <luke.webster86@gmail.com>
2022-03-08 15:04:56 +0000

    Split starter tier into old and new (inc/exc vat)

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,6 @@
 class Plan < ApplicationRecord
   def self.public
-    where.not(name: 'starter_dep')
+    where.not(name: ['starter_dep', 'starter (2021)'])
       .where(interval: 'month')
       .order(:amount)
   end
@@ -9,6 +9,7 @@ class Plan < ApplicationRecord
     case name.downcase
     when 'starter' then 30
     when 'starter_dep' then 30
+    when 'starter (2021)' then 30
     when 'standard' then 200
     when 'enterprise' then 1000
     else 0

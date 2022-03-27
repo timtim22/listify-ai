ab1258bd7d68620169da04996a64df5b219c6ca4
Luke Webster <luke.webster86@gmail.com>
2022-03-27 13:09:18 +0100

    Export descriptions only

--- app/controllers/admin/data_exports_controller.rb
+++ app/controllers/admin/data_exports_controller.rb
@@ -4,19 +4,21 @@ class Admin::DataExportsController < ApplicationController
   def index
     model_types = %w[Listing]
     admin_user_ids = User.where(admin: true).pluck(:id)
-    task_runs = TaskRun.where(input_object_type: model_types).where.not(user_id: admin_user_ids).includes(:task_results)
+    task_runs = TaskRun.where(input_object_type: model_types).where.not(user_id: admin_user_ids).includes(:task_results, :input_object)
     headers = %w[user_id task_run_id created_at input_object_type prompt completion]
     full_csv = CSV.generate(headers: true) do |csv|
       csv << headers
 
       task_runs.each do |t|
+        next if t.input_object.request_type != 'listing_description'
+
         csv << [
           t.user_id,
           t.id,
           t.created_at,
           t.input_object_type,
           t.input_object.input_text,
-          t.task_results&.first&.result_text
+          t.task_results.map(&:result_text).join('\n-----\n')
         ]
       end
     end

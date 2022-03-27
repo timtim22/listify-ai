1a12eb09c5fb4de4f193fa0fa63fb535e76d00ad
Luke Webster <luke.webster86@gmail.com>
2022-03-27 12:49:34 +0100

    Add user ids to export csv

--- app/controllers/admin/data_exports_controller.rb
+++ app/controllers/admin/data_exports_controller.rb
@@ -5,12 +5,19 @@ class Admin::DataExportsController < ApplicationController
     model_types = %w[Listing]
     admin_user_ids = User.where(admin: true).pluck(:id)
     task_runs = TaskRun.where(input_object_type: model_types).where.not(user_id: admin_user_ids).includes(:task_results)
-    headers = %w[task_run_id created_at input_object_type prompt completion]
+    headers = %w[user_id task_run_id created_at input_object_type prompt completion]
     full_csv = CSV.generate(headers: true) do |csv|
       csv << headers
 
       task_runs.each do |t|
-        csv << [t.id, t.created_at, t.input_object_type, t.input_object.input_text, t.task_results&.first&.result_text]
+        csv << [
+          t.user_id,
+          t.id,
+          t.created_at,
+          t.input_object_type,
+          t.input_object.input_text,
+          t.task_results&.first&.result_text
+        ]
       end
     end
     respond_to do |format|

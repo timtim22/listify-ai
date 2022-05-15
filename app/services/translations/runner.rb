7e1c1570d037482b6385886b5a110bd87c92b059
Luke Webster <luke.webster86@gmail.com>
2022-05-15 14:40:36 +0100

    Refactor out translate to English

--- app/services/translations/runner.rb
+++ app/services/translations/runner.rb
@@ -6,5 +6,24 @@ module Translations
         Translation.create_for!(task_result, response)
       end
     end
+
+    def request_in_english(language, input_text)
+      if language.present? && language != 'EN'
+        translation = translate_to_english!(language, input_text)
+        {
+          input_language: language,
+          untranslated_input_text: input_text,
+          input_text: translation[:error] ? '' : translation[:text]
+        }
+      else
+        {}
+      end
+    end
+
+    private
+
+    def translate_to_english!(language, input_text)
+      ApiClients::DeepL.new.translate(language, 'EN-GB', input_text)
+    end
   end
 end

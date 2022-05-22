219b362fec942d6ce045163819143152841bc5ee
Luke Webster <luke.webster86@gmail.com>
2022-05-22 19:25:32 +0100

    Temp move shared helper method to app helper as helpers no longer shared

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,35 @@ module ApplicationHelper
   def formatted_amount(amount, options = { unit: '£', precision: 2 })
     number_to_currency(amount.to_i / 100.0, options)
   end
+
+  TAGLINES = {
+    starter: 'Perfect for property managers just getting started',
+    standard: 'Perfect for small teams with a growing portfolio',
+    premium: 'Perfect for established teams working at scale',
+    enterprise: 'Customise Listify to your style and needs'
+  }
+
+  SPIN_COUNT = { #move to db
+    starter: 30,
+    standard: 200,
+    premium: 1000
+  }
+
+  SEAT_COUNT = {
+    starter: 1,
+    standard: 3,
+    premium: 5
+  }
+
+  def tagline_for(plan_name)
+    TAGLINES[plan_name.downcase.to_sym]
+  end
+
+  def spin_count_for(plan_name)
+    SPIN_COUNT[plan_name.downcase.to_sym]
+  end
+
+  def seat_count_for(plan_name)
+    SEAT_COUNT[plan_name.downcase.to_sym]
+  end
 end

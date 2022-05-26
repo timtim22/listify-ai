b8a4c39901f449be5081e6c93f7335abd413b25d
Luke Webster <luke.webster86@gmail.com>
2022-05-26 15:41:51 +0100

    Catch error string (likely internal 500)

--- app/javascript/components/common/ErrorNotice.js
+++ app/javascript/components/common/ErrorNotice.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const catch500 = 'Oops! Something went wrong on our side. We\'re sorry about this, and we hopefully have been alerted, but please let us know you\'re seeing this.'
 
 const ErrorNotice = ({ errors }) => {
+  const [unexpectedError, setUnexpectedError] = useState(false);
 
   const parseErrors = () => {
     let errorMessages = [];
-    if (errors.message) {
+    if (typeof(errors) === 'string') {
+      errorMessages.push(catch500);
+      if (!unexpectedError) {
+        setUnexpectedError(true);
+      }
+    } else if (errors.message) {
       errorMessages.push(errors.message);
     } else {
       Object.keys(errors).forEach((key) => {
@@ -29,7 +38,7 @@ const ErrorNotice = ({ errors }) => {
 
     return (
       <div className="p-4 mb-4 text-red-800 bg-red-100 rounded border border-red-200">
-        <h2>Please correct the following errors:</h2>
+        {!unexpectedError && <h2>Please correct the following errors:</h2>}
         <ul className="py-2">
           {errorMessages.map(msg => renderError(msg))}
         </ul>
@@ -39,6 +48,13 @@ const ErrorNotice = ({ errors }) => {
     return null;
   }
 
+}
+
+ErrorNotice.propTypes = {
+  errors: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ])
 }
 
 export default ErrorNotice;

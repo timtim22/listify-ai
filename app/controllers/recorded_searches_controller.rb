228d8b5bc9b1229fff7a5a64eb4ec68b90669b9b
Luke Webster <luke.webster86@gmail.com>
2022-06-02 09:25:22 +0100

    Add search to recorded search table

--- app/controllers/recorded_searches_controller.rb
+++ app/controllers/recorded_searches_controller.rb
@@ -2,8 +2,13 @@ class RecordedSearchesController < ApplicationController
   before_action :authenticate_admin
 
   def index
-    @recorded_searches = RecordedSearch
+    @q = RecordedSearch.ransack(params[:q])
+
+    @recorded_searches = @q
+      .result
       .includes(:user, :search_location)
       .order(created_at: :desc)
+
+    @pagy, @recorded_searches = pagy(@recorded_searches, items: 30)
   end
 end

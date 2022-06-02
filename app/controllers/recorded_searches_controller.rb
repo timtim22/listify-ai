31e9afef966750f4983174cca6dd96f8d2919170
Luke Webster <luke.webster86@gmail.com>
2022-06-02 09:42:45 +0100

    Show search statistics

--- app/controllers/recorded_searches_controller.rb
+++ app/controllers/recorded_searches_controller.rb
@@ -10,5 +10,15 @@ class RecordedSearchesController < ApplicationController
       .order(created_at: :desc)
 
     @pagy, @recorded_searches = pagy(@recorded_searches, items: 30)
+    assign_statistics
+  end
+
+  private
+
+  def assign_statistics
+    @statistics = OpenStruct.new(
+      last_1_day: RecordedSearch.since(24.hours.ago).count,
+      last_7_days: RecordedSearch.since(7.days.ago).count,
+    )
   end
 end
--- app/models/recorded_search.rb
+++ app/models/recorded_search.rb
@@ -1,4 +1,8 @@
 class RecordedSearch < ApplicationRecord
   belongs_to :search_location
   belongs_to :user
+
+  def self.since(time)
+    where('created_at > ?', time)
+  end
 end

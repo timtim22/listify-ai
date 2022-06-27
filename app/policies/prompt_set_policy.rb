1a357b4840688fe2257ea0f9cb4b7deab6fef167
Luke Webster <luke.webster86@gmail.com>
2022-06-27 20:58:29 +0100

    Update access to prompts

---
+++ app/policies/prompt_set_policy.rb
@@ -0,0 +1,36 @@
+class PromptSetPolicy < ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    user.admin? || (user.id == '215ef9f4-359f-4bdd-972b-e16940c1e7a3')
+  end
+
+  def show?
+    index?
+  end
+
+  def create?
+    index?
+  end
+
+  def new?
+    index?
+  end
+
+  def update?
+    index?
+  end
+
+  def edit?
+    index?
+  end
+
+  def destroy?
+    index?
+  end
+end

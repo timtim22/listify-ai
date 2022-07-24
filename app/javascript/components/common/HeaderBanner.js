bda5c2f3b14a8ffa53e110eb5d23e81de3420255
Luke Webster <luke.webster86@gmail.com>
2022-07-24 07:57:40 +0100

    Change color on trial expired banner

--- app/javascript/components/common/HeaderBanner.js
+++ app/javascript/components/common/HeaderBanner.js
@@ -47,15 +47,19 @@ export const TrialExpiredBanner = () => {
     <Banner
       title="Your trial has expired"
       text='You are limited to 5 spins per month. To access more, subscribe or contact hello@listify.ai.'
+      noticeLevel='warning'
     />
   )
 }
 
-const Banner = ({ title, text }) => {
+const Banner = ({ title, text, noticeLevel = 'info' }) => {
+  const borderColor = noticeLevel === 'warning' ? 'border-orange-500' : 'border-teal-500';
+  const iconColor   = noticeLevel === 'warning' ? 'text-orange-500' : 'text-teal-500';
+
   return (
-    <div className="py-3 px-4 text-left rounded-b border-l-4 border-teal-500 shadow border-t-1" role="alert">
+    <div className={`py-3 px-4 text-left rounded-b border-l-4 ${borderColor} shadow border-t-1`} role="alert">
       <div className="flex items-center">
-        <div className="py-1 text-teal-500">
+        <div className={`py-1 ${iconColor}`}>
           <svg className="mr-4 w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
           </svg>
@@ -75,5 +79,6 @@ TrialActiveBanner.propTypes = {
 
 Banner.propTypes = {
   title: PropTypes.string,
-  text: PropTypes.string
+  text: PropTypes.string,
+  noticeLevel: PropTypes.string
 };

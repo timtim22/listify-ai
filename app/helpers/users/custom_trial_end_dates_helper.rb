a1570d6ae4cebe489fa1be6e6ac478245fb8890c
Luke Webster <luke.webster86@gmail.com>
2022-07-24 07:41:20 +0100

    refactor for brevity

--- app/helpers/users/custom_trial_end_dates_helper.rb
+++ app/helpers/users/custom_trial_end_dates_helper.rb
@@ -1,14 +1,6 @@
 module Users::CustomTrialEndDatesHelper
   def dates_to_extend_trial_options
-    today = Time.zone.today
-    opts = [
-      today + 1.week,
-      today + 2.weeks,
-      today + 3.weeks,
-      today + 4.weeks,
-      today + 5.weeks,
-      today + 6.weeks
-    ]
+    opts = (1..6).map { |i| Time.zone.today + i.weeks }
     opts.map { |date| [format_date_for_display(date), date] }
   end
 end

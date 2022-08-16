1437533f66190b93b7a2714efd6852b70b46c4cd
Luke Webster <luke.webster86@gmail.com>
2022-08-16 16:38:47 +0100

    Switch output language sets based on enabled modules

--- app/javascript/components/common/LanguageToggle.js
+++ app/javascript/components/common/LanguageToggle.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { supportedLanguages } from '../../helpers/translations';
+import { UserContext } from '../listings/New';
+import { outputLanguagesForUser } from '../../helpers/translations';
 
 const LanguageToggle = ({ languageVisible, toggleVisible }) => {
+  const user = useContext(UserContext);
 
   return (
     <select
       value={languageVisible}
       onChange={(e) => toggleVisible(e.target.value)}
       className="block mx-3 w-full text-xs bg-gray-50 rounded-md border-gray-200 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-      {supportedLanguages.map((item) => {
+      {outputLanguagesForUser(user).map((item) => {
         return (
           <option key={item.name} value={item.value}>{item.value}</option>
         )

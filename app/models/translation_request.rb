c7780c8ad234eec93e74c6d19adcb2916db4e262
Luke Webster <luke.webster86@gmail.com>
2022-08-17 21:02:25 +0100

    Fix translations list on backend

--- app/models/translation_request.rb
+++ app/models/translation_request.rb
@@ -1,7 +1,7 @@
 class TranslationRequest < ApplicationRecord
   belongs_to :task_run
 
-  OUTPUT_LANGUAGES = [ "DA", "FR", "DE", "IT", "ES", "NL" ]
+  OUTPUT_LANGUAGES = %w[DA DE ES FR IT NL RO RU SR SR-LATN ZH].freeze
 
   def self.create_for!(output_language)
     if OUTPUT_LANGUAGES.include?(output_language)

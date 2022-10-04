9d423c0f900c73cc899edaaa4e65bdbf7e233e31
Cath Garcia <cath@listify.ai>
2022-10-04 13:01:17 +0800

    Add checking when removing a user from the team

--- app/controllers/team_roles_controller.rb
+++ app/controllers/team_roles_controller.rb
@@ -6,10 +6,14 @@ class TeamRolesController < ApplicationController
     @team_role = TeamRole.find(params[:id])
     team_id = @team_role.team_id
     respond_to do |format|
-      if @team_role.destroy
-        format.html { redirect_to team_path(team_id), notice: 'User removed from the team successfully.' }
+      if @team_role.admin_privileges?
+        format.html { rredirect_to team_path(team_id), alert: 'Removal of admin is not allowed.' }
       else
-        format.html { rredirect_to team_path(team_id), alert: 'An error occurred while removing the user from the team.' }
+        if @team_role.destroy
+          format.html { redirect_to team_path(team_id), notice: 'User removed from the team successfully.' }
+        else
+          format.html { rredirect_to team_path(team_id), alert: 'An error occurred while removing the user from the team.' }
+        end
       end
     end
   end

987ca4b24ce2ac3941c3b8cd379bd4eed26fce9c
Cath Garcia <cath@listify.ai>
2022-12-05 17:39:09 +0800

    Fix notices for reconfirm email

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,9 +8,17 @@ class AccountsController < ApplicationController
   def update
     @user.assign_attributes(account_params)
 
+    notice =
+      if @user.email_changed?
+        'A message with a confirmation link has been sent to your email address. ' \
+          'Please follow the link to activate your account before you can use your updated email to login.'
+      else
+        'Account updated successfully.'
+      end
+
     respond_to do |format|
       if @user.save
-        format.html { redirect_to edit_account_path, notice: 'Account updated successfully' }
+        format.html { redirect_to edit_account_path, notice: notice }
         format.json { render :edit, status: :updated, location: edit_account_path }
       else
         format.html { render :edit, status: :unprocessable_entity }

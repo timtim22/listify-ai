8c880dc4f606dd311ae505eaeeed54444a52d778
Luke Webster <luke.webster86@gmail.com>
2022-12-11 11:06:36 +0000

    Fix migrations (one typo was causing an error)

--- db/migrate/20221118152558_change_step_pompt_id_data_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStepPomptIdDataType < ActiveRecord::Migration[6.1]
-  def change
-    add_column :task_results, :step_prompt_id, :string
-  end
-end
--- db/migrate/20221124115117_add_procedurereference_to_task_results.rb
+++
@@ -1,5 +0,0 @@
-class AddProcedurereferenceToTaskResults < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :task_results, :procedure
-  end
-end
